ce5cd8adddb7b60a3533922c4e942d749e3f6d9c
huangup <clark.kent.hnh@gmail.com>
2021-02-02 17:00:33 +0800

    Update demo.ts

--- demo.ts
+++ demo.ts
@@ -1,34 +1,24 @@
 import { mockDataList, mockTreeList } from './index';
 
-/** table rows mock */
-export const mockTableRows = mockDataList(100, index => ({
+export const dataList = mockDataList(100, (index) => ({
   name: `name-${index}`,
-  desc: `desc-${index}`,
-  alias: `mingcheng-${index}`,
+  cname: `mingcheng-${index}`,
   owner: `owner-${index}`,
-  id: `id-${index}`,
+  id: `id-${index}`
 }));
 
-/** list data mock  */
-export const mockAssetsListIndexData = mockDataList(100, index => ({
-  name: `name-${index}`,
-  id: `id-${index}`,
-}));
-
-/** tree data mock */
-export const mockTopicAssetsTreeList = mockTreeList(
-  (level, index, key, parentKey, path, isLeaf) => ({
-    title: key,
-    data: {
-      level,
-      index,
-      key,
-      parentKey,
-      path,
-      isLeaf,
-      associated: false,
-    },
+const LEVEL_LIMIT = 5;
+export const treeList = mockTreeList(
+  (level, index, key, path, parentKey, parentPath, isLeaf) => ({
+    level,
+    index,
+    key,
+    path,
+    parentKey,
+    parentPath,
+    isLeaf,
+    name: key
   }),
-  5,
-  { 0: 5, 1: 7, 2: 3, 3: 2, 4: 4 },
+  LEVEL_LIMIT,
+  { 0: 5, 1: 7, 2: 3, 3: 2, 4: 4 }
 );
